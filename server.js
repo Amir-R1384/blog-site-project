ad8c12651e34578ea7cca5f1a68cb71b17355155
Amir-R1384 <ranjbaramirreza5786@yahoo.com>
2021-08-30 18:25:39 -0400

    switched from pug to ejs and did some cleaning

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-if (process.env.NODE_ENV === "development") require("dotenv").config()
+if (process.env.NODE_ENV !== "production") require("dotenv").config()
+
 const express = require("express")
+const path = require("path")
+
 const app = express()
 
-const indexRouter = require("./routes/index.js")
 
-app.set("view engine", "pug")
+app.set("view engine", "ejs")
 app.use(express.static("src"))
 
 
-app.use("/", indexRouter)
+app.get('/', (req, res) => {
+    try {
+        res.render('index')
 
-app.listen(process.env.PORT || 3000, () => console.log("server is live"))
+    } catch(error) {
+
+        console.error(error)
+        res.render("error", {error})
+        
+    }
+    
+})
+
+app.listen(process.env.PORT, () => console.log("server is live"))
